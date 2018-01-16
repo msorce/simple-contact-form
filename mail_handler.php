105d6d26d407bc9c430da5c1f89288052272664e
mrk <msorce@users.noreply.github.com>
2018-01-16 11:19:21 -0800

    Update mail_handler.php

--- mail_handler.php
+++ mail_handler.php
@@ -16,7 +16,7 @@ if(isset($_POST['submit'])){
         die();
     }
 
-    $to = "iamsorce@gmail.com"; // this is your Email address
+    $to = "<email>; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
